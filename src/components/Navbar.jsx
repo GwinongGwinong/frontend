7b6212d20e8ae68563dc7d0fca6b7f1b75270685
Jimin0430 <tangbole0430@gmail.com>
2023-08-11 23:50:09 +0900

    기초세팅2

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React, { useContext, useEffect, useState } from "react";
+import styles from "../styles/Navbar.module.css";
+
+export default function Navbar() {
+    
+    return (
+        <>
+        
+        </>
+    );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React, { useContext, useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+
+    return (
+        <>
+        <Navbar />
+
+        </>
+    );
+        
+}
