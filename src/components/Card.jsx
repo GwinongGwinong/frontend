00a60ff0c16b4961169445eb6781fece10efae66
Kim Min Jeong <dotoriido@gmail.com>
2023-08-21 14:31:45 +0900

    Update main page & Remove slider

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Card() {
+    return (
+        <div>
+            <img src="" alt="" />
+            <div>
+                <div>벼농사 재배 경운기</div>
+                <div>전라북도 부안군</div>
+                <div>2023년 8월 10일 ~ 2024년 1월 10일</div>
+                <div>월230/6개월</div>
+            </div>
+        </div>
+    );
+}
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,26 @@
 import React, { useContext, useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 import Navbar from "../components/Navbar";
-import ImageSlider from "../components/Slider";
-
+//import ImageSlider from "../components/Slider";
+import Titles from "../components/Titles";
 export default function Home() {
 
     return (
         <>
         <Navbar />
-        <ImageSlider />
+        <Titles />
+        <div>
+            인기있는 파밍 홀리데이 패키지
+        </div>
+        <div>
+            살아보고 싶은 이색 농촌 전원 주택
+        </div>
+        <div>
+            파밍 홀리데이 순간들
+        </div>
+        <div>
+            파밍 홀리데이 슈퍼 호스트
+        </div>
         </>
     );
         
