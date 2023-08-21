9da2cb9077ffe09a6bbcd050f455b6035a71701a
Kim Min Jeong <dotoriido@gmail.com>
2023-08-21 17:27:23 +0900

    Complete card component template
    card component의 인자를 외부에서 받아오는 것 구현이 필요하다.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import styles from '../styles/Card.module.css'
 export default function Card() {
     return (
-        <span className={styles.span}>
-            <div className={styles.wrap}>
+        <div className={styles.div}>
+            <div className={styles.wrapImg}>
                 <div><img src={`${process.env.PUBLIC_URL}/public_assets/package-1.png`} /></div>
                 <div className={styles.heart}><img className={styles.img} src={`${process.env.PUBLIC_URL}/public_assets/heart.png`} /></div>
                 <div className={styles.text}><p>모집중</p></div>
             </div>
-            <div>
+            <div className={styles.wrapText}>
                 <div>벼농사 재배 경운기</div>
                 <div>전라북도 부안군</div>
                 <div>2023년 8월 10일 ~ 2024년 1월 10일</div>
                 <div>월230/6개월</div>
             </div>
-        </span>
+        </div>
     );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,24 +10,43 @@ export default function Home() {
         <>
         <Navbar />
         {/* <ImageSlider /> */}
-        <div>
+        <div className={styles.titles}>
             <a href='#one'>홀리데이 패키지</a>
             <a href='#two'>이색 전원주택</a>
             <a href='#three'>홀리데이 커뮤니티</a>
             <a href='#four'>호스트 이야기</a>
         </div>
         <div id="one">
-            <div>인기있는 파밍 홀리데이 패키지</div>
-            <Card />
+            <h2>인기있는 파밍 홀리데이 패키지</h2>
+            <div className={styles.card}>
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </div>
         <div id="two">
-            <div>살아보고 싶은 이색 농촌 전원 주택</div>
+            <h2>살아보고 싶은 이색 농촌 전원 주택</h2>
+            <div className={styles.card}>
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </div>
         <div id="three">
-            <div>파밍 홀리데이 순간들</div>
+            <h2>파밍 홀리데이 순간들</h2>
+            <div className={styles.card}>
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </div>
         <div id="four">
-            <div>파밍 홀리데이 슈퍼 호스트</div>
+            <h2>파밍 홀리데이 슈퍼 호스트</h2>
+            <div className={styles.card}>
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </div>
         </>
     );
