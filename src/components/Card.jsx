51cf9a46e69599c6c9b5d38dd0f0624b0138905c
Kim Min Jeong <dotoriido@gmail.com>
2023-08-25 14:09:13 +0900

    Update Card component - 외부입력 받기 가능
    모든 요소를 파라미터로 받게 한다.
    Card Component의 재활용을 위하여!
    해당되지 않는 것들을 적지 않는 방향으로 진행한다.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import styles from '../styles/Card.module.css'
-export default function Card({imageTop,imageUnder,textTop,textTopRight,textMiddle1,textMiddle2,textUnderLeft,textUnderRight}) {
+export default function Card({image,imageTop,imageUnder,textTop,textTopRight,textMiddle1,textMiddle2,textUnderLeft,textUnderRight}) {
     return (
         <div className={styles.div}>
             <div className={styles.wrapImg}>
-                <div><img src={`${process.env.PUBLIC_URL}/public_assets/package-1.png`} /></div>
-                <div className={styles.heart}><img className={styles.img} src={`${process.env.PUBLIC_URL}/public_assets/heart.png`} /></div>
-                <div className={styles.text}><div>모집중</div></div>
+                <div><img src={image} /></div>
+                <div className={styles.heart}><img className={styles.img} src={imageTop} /></div>
+                <div className={styles.text}><div>{imageUnder}</div></div>
             </div>
             <div className={styles.wrapText}>
-                <div>벼농사 재배 경운기</div>
-                <div>전라북도 부안군</div>
-                <div>2023년 8월 10일 ~ 2024년 1월 10일</div>
-                <div className={styles.bottomRight}>월230/6개월</div>
+                <div>{textTop}</div>
+                <div>{textMiddle1}</div>
+                <div>{textMiddle2}</div>
+                <div className={styles.bottomRight}>{textUnderRight}</div>
             </div>
         </div>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,13 @@ import ImageSlider from "../components/Slider";
 import Card from "../components/Card";
 export default function Home() {
     const [imgUrl,setImgUrl] = useState(['main','main','main']);
-
+    /*
+    Card Component
+    : imageTop,imageUnder,
+    textTop,textTopRight,
+    textMiddle1,textMiddle2,
+    textUnderLeft,textUnderRight
+    */
     return (
         <>
         <Navbar />
@@ -19,8 +25,21 @@ export default function Home() {
         <div id="one">
             <h2>인기있는 파밍 홀리데이 패키지</h2>
             <div className={styles.card}>
-                <Card />
-                <Card />
+                <Card
+                    image={`${process.env.PUBLIC_URL}/public_assets/package-1.png`}
+                    imageTop={`${process.env.PUBLIC_URL}/public_assets/heart.png`}
+                    imageUnder={`모집중`}
+                    textTop={`벼농사 재배 경운기`}
+                    textMiddle1={`전라북도 부안군`}
+                    textMiddle2={`2023년 8월 10일 ~ 2024년 1월 10일`}
+                    textUnderRight={`월230/6개월`}/>
+                <Card
+                    imageTop={`${process.env.PUBLIC_URL}/public_assets/heart.png`}
+                    imageUnder={`모집중`}
+                    textTop={`치즈 유가공`}
+                    textMiddle1={`전라북도 임실군`}
+                    textMiddle2={`2023년 8월 20일 ~ 2024년 1월 20일`}
+                    textUnderRight={`월220/7개월`}/>
                 <Card />
             </div>
         </div>
